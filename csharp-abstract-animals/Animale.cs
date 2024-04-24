6ee70461b07101e7f9171ec4a62149d18ab227d8
enricoforesta <enrico.foresta97@gmail.com>
2024-04-24 14:21:43 +0200

    Aggiungere i file di progetto.

---
+++ csharp-abstract-animals/Animale.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    public abstract class Animale
+    {
+
+    }
+
+    public class Cane
+    {
+
+    }
+    public class Passerotto
+    {
+
+    }
+    public class Aquila
+    {
+
+    }
+    public class Delfino
+    {
+
+    }
+    
+}
